12ec4d58621ad59364d70579ea8e1b49872351ff
Masho Ado <mashoado@gmail.com>
2021-12-04 00:13:07 +0300

    create the first 11 component with mock data

---
+++ client/src/Components/First11.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import '../styles/First11.css'
+
+
+function First11() {
+    return (
+        <section className="first11-section">
+            <div className="first11-container">
+
+                <div className='goalkeeper-container'>
+
+                    <div className="goalkeeper">
+                        <image></image>
+                        <div className="player-info">
+                            <div className="player-name">Mendy</div>
+                            <div className="next-opponent">WHU(A)</div>
+                        </div>
+                    </div> 
+
+                </div>
+
+                <div className="defender-container">
+
+                    <div className="defender">
+                        <img src="" alt="" />
+                        <div className="player-info">
+                            <div className="player-name">Cancelo</div>
+                            <div className="next-opponent">WHU(A)</div>
+                        </div>
+                    </div>
+
+                    <div className="defender">
+                        <img src="" alt="" />
+                        <div className="player-info">
+                            <div className="player-name">James</div>
+                            <div className="next-opponent">WHU(A)</div>
+                        </div>
+                    </div>
+
+                    <div className="defender">
+                        <img src="" alt="" />
+                        <div className="player-info">
+                            <div className="player-name">Livramento</div>
+                            <div className="next-opponent">WHU(A)</div>
+                        </div>
+                    </div>
+
+                    <div className="defender">
+                        <img src="" alt="" />
+                        <div className="player-info">
+                            <div className="player-name">Trent</div>
+                            <div className="next-opponent">WHU(A)</div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="midfielders-container">
+                
+                    <div className="midfielders">
+                        <img src="" alt="" />
+                        <div className="player-info">
+                            <div className="player-name">Gilmour</div>
+                            <div className="next-opponent">WHU(A)</div>
+                        </div>
+                    </div>
+
+                    <div className="midfielders">
+                        <img src="" alt="" />
+                        <div className="player-info">
+                            <div className="player-name">Son</div>
+                            <div className="next-opponent">WHU(A)</div>
+                        </div>
+                    </div>
+
+                    <div className="midfielders">
+                        <img src="" alt="" />
+                        <div className="player-info">
+                            <div className="player-name">Salah</div>
+                            <div className="next-opponent">WHU(A)</div>
+                        </div>
+                    </div>
+
+                    <div className="midfielders">
+                        <img src="" alt="" />
+                        <div className="player-info">
+                            <div className="player-name">Gallagher</div>
+                            <div className="next-opponent">WHU(A)</div>
+                        </div>
+                    </div>
+
+                    
+                </div>
+
+                <div className="fowards-container">
+
+                    <div className="fowards">
+                        <img src="" alt="" />
+                        <div className="player-info">
+                            <div className="player-name">Vardy</div>
+                            <div className="next-opponent">WHU(A)</div>
+                        </div>
+                    </div>
+
+                    <div className="fowards">
+                        <img src="" alt="" />
+                        <div className="player-info">
+                            <div className="player-name">Jimenez</div>
+                            <div className="next-opponent">WHU(A)</div>
+                        </div>
+                    </div>
+
+                </div>
+                
+            </div>
+        </section>
+    )
+}
+
+export default First11
