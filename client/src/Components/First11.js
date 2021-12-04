8ee02eaca3e8229492bfc5cf090d164dbcfd111f
Masho Ado <mashoado@gmail.com>
2021-12-04 12:33:45 +0300

    create pitch and fit in the mock players in the appropriate sections of the pitch

--- client/src/Components/First11.js
+++ client/src/Components/First11.js
@@ -1,120 +1,128 @@
-import React from 'react'
-import '../styles/First11.css'
-
+import React from "react";
+import "../styles/First11.css";
 
 function First11() {
-    return (
-        <section className="first11-section">
-            <div className="first11-container">
+	return (
+		<section className="first11-section">
+			<h2 className="section-heading">Select Your Best 11</h2>
+			<div className="pitch-container">
+				<div className="pitch">
+					{/* first 11 container */}
+					{/* <!-- the grass is green here --> */}
 
-                <div className='goalkeeper-container'>
+					<div className="lines">
+						{/* <!-- the outside lines of the pitch --> */}
+						<span className="corner corner-top-left">
+							{/* <!-- the top left corner --> */}
+						</span>
+						<span className="corner corner-top-right">
+							{/* <!-- the top right corner --> */}
+						</span>
 
-                    <div className="goalkeeper">
-                        <image></image>
-                        <div className="player-info">
-                            <div className="player-name">Mendy</div>
-                            <div className="next-opponent">WHU(A)</div>
-                        </div>
-                    </div> 
+						<div className="goalBox">
+							{/* <!-- the goal box goes here --> */}
+							<div className="goalkeeper-container">
+								<div className="goalkeeper">
+									<img src="" alt="" />
+									<div className="player-info">
+										<div className="player-name">Mendy</div>
+										<div className="next-opponent">WHU(A)</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="defender-container">
+							<div className="defender">
+								<img src="" alt="" />
+								<div className="player-info">
+									<div className="player-name">Cancelo</div>
+									<div className="next-opponent">WHU(A)</div>
+								</div>
+							</div>
 
-                </div>
+							<div className="defender">
+								<img src="" alt="" />
+								<div className="player-info">
+									<div className="player-name">James</div>
+									<div className="next-opponent">WHU(A)</div>
+								</div>
+							</div>
 
-                <div className="defender-container">
+							<div className="defender">
+								<img src="" alt="" />
+								<div className="player-info">
+									<div className="player-name">Livramento</div>
+									<div className="next-opponent">WHU(A)</div>
+								</div>
+							</div>
 
-                    <div className="defender">
-                        <img src="" alt="" />
-                        <div className="player-info">
-                            <div className="player-name">Cancelo</div>
-                            <div className="next-opponent">WHU(A)</div>
-                        </div>
-                    </div>
+							<div className="defender">
+								<img src="" alt="" />
+								<div className="player-info">
+									<div className="player-name">Trent</div>
+									<div className="next-opponent">WHU(A)</div>
+								</div>
+							</div>
+						</div>
 
-                    <div className="defender">
-                        <img src="" alt="" />
-                        <div className="player-info">
-                            <div className="player-name">James</div>
-                            <div className="next-opponent">WHU(A)</div>
-                        </div>
-                    </div>
+						<div className="midfielders-container">
+							<div className="midfielders">
+								<img src="" alt="" />
+								<div className="player-info">
+									<div className="player-name">Gilmour</div>
+									<div className="next-opponent">WHU(A)</div>
+								</div>
+							</div>
 
-                    <div className="defender">
-                        <img src="" alt="" />
-                        <div className="player-info">
-                            <div className="player-name">Livramento</div>
-                            <div className="next-opponent">WHU(A)</div>
-                        </div>
-                    </div>
+							<div className="midfielders">
+								<img src="" alt="" />
+								<div className="player-info">
+									<div className="player-name">Son</div>
+									<div className="next-opponent">WHU(A)</div>
+								</div>
+							</div>
 
-                    <div className="defender">
-                        <img src="" alt="" />
-                        <div className="player-info">
-                            <div className="player-name">Trent</div>
-                            <div className="next-opponent">WHU(A)</div>
-                        </div>
-                    </div>
+							<div className="midfielders">
+								<img src="" alt="" />
+								<div className="player-info">
+									<div className="player-name">Salah</div>
+									<div className="next-opponent">WHU(A)</div>
+								</div>
+							</div>
 
-                </div>
+							<div className="midfielders">
+								<img src="" alt="" />
+								<div className="player-info">
+									<div className="player-name">Gallagher</div>
+									<div className="next-opponent">WHU(A)</div>
+								</div>
+							</div>
+						</div>
+						<div className="fowards-container">
+							<div className="fowards">
+								<img src="" alt="" />
+								<div className="player-info">
+									<div className="player-name">Vardy</div>
+									<div className="next-opponent">WHU(A)</div>
+								</div>
+							</div>
 
-                <div className="midfielders-container">
-                
-                    <div className="midfielders">
-                        <img src="" alt="" />
-                        <div className="player-info">
-                            <div className="player-name">Gilmour</div>
-                            <div className="next-opponent">WHU(A)</div>
-                        </div>
-                    </div>
-
-                    <div className="midfielders">
-                        <img src="" alt="" />
-                        <div className="player-info">
-                            <div className="player-name">Son</div>
-                            <div className="next-opponent">WHU(A)</div>
-                        </div>
-                    </div>
-
-                    <div className="midfielders">
-                        <img src="" alt="" />
-                        <div className="player-info">
-                            <div className="player-name">Salah</div>
-                            <div className="next-opponent">WHU(A)</div>
-                        </div>
-                    </div>
-
-                    <div className="midfielders">
-                        <img src="" alt="" />
-                        <div className="player-info">
-                            <div className="player-name">Gallagher</div>
-                            <div className="next-opponent">WHU(A)</div>
-                        </div>
-                    </div>
-
-                    
-                </div>
-
-                <div className="fowards-container">
-
-                    <div className="fowards">
-                        <img src="" alt="" />
-                        <div className="player-info">
-                            <div className="player-name">Vardy</div>
-                            <div className="next-opponent">WHU(A)</div>
-                        </div>
-                    </div>
-
-                    <div className="fowards">
-                        <img src="" alt="" />
-                        <div className="player-info">
-                            <div className="player-name">Jimenez</div>
-                            <div className="next-opponent">WHU(A)</div>
-                        </div>
-                    </div>
-
-                </div>
-                
-            </div>
-        </section>
-    )
+							<div className="fowards">
+								<img src="" alt="" />
+								<div className="player-info">
+									<div className="player-name">Jimenez</div>
+									<div className="next-opponent">WHU(A)</div>
+								</div>
+							</div>
+						</div>
+						<div className="half">
+							{/* <!-- the half way point of the pitch is here --> */}
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
-export default First11
+export default First11;
