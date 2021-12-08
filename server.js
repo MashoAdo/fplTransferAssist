f2dd6ce51f751911762212b563d42c064a2a7406
Masho Ado <mashoado@gmail.com>
2021-12-08 20:46:05 +0300

    fetch fixtures from fpl fixtures api

--- server.js
+++ server.js
@@ -1,28 +1,46 @@
-const express = require('express')
-const request = require('request')
-const cors = require('cors')
+const express = require("express");
+const request = require("request");
+const cors = require("cors");
 
-const app = express()
-app.use(cors())
+const app = express();
+var corsOptions = {
+	origin: "http://127.0.0.1:5500/client/public/index.html",
+};
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send('working')
-})
+	res.sendFile(__dirname + "/index.html");
+});
 
 app.get("/test", (req, res) => {
-    res.json({ 'msg': 'connected to backend' })
-})
+	res.json({ msg: "connected to backend" });
+});
 
 app.get("/fetchbootstrap", (req, res) => {
-    request("https://fantasy.premierleague.com/api/bootstrap-static/", function (error, response, body) {
-        if (error || response.statusCode !== 200) {
-            return res.status(500).json({ type: 'error', message: err.message });
-        }
+	request(
+		"https://fantasy.premierleague.com/api/bootstrap-static/",
+		function (error, response, body) {
+			if (error || response.statusCode !== 200) {
+				return res.status(500).json({ type: "error", message: error.message });
+			}
 
-        res.send(body)
-    })
-})
+			res.send(body);
+		}
+	);
+});
+
+app.get("/fixtures", (req, res) => {
+	request(
+		"https://fantasy.premierleague.com/api/fixtures/?future=1",
+		(error, response, body) => {
+			if (error || response.statusCode !== 200) {
+				return res.status(500).json({ typer: "error", message: error.message });
+			}
+			res.send(body);
+		}
+	);
+});
 
 app.listen(4001, () => {
-    console.log('server is running at port 4001')
-})
+	console.log("server is running at port 4001");
+});
