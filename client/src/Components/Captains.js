00d3497de0e59c64e5577b91ffc589ff03ab444a
Masho Ado <mashoado@gmail.com>
2021-12-10 22:40:52 +0300

    add team and player ame info in the captain info container

--- client/src/Components/Captains.js
+++ client/src/Components/Captains.js
@@ -4,60 +4,181 @@ import "../styles/Captains.css";
 function Captains() {
 	return (
 		<section className="captains-section">
-			<div className="captain">
-				<div className="captain-image"></div>
-				<div className="captain-info">
-					<div className="form">
-						<div className="info-name">
-							<span>Name</span>
+			<h1 className="section-heading">Top 3 Captains this Week</h1>
+			<div className="captains-container">
+				<div className="captain">
+					<div className="captain-image"></div>
+					<div className="captain-info">
+						<div className="form">
+							<div className="info-name">
+								<span>Name</span>
+							</div>
+							<div className="info-stat">
+								<span>T.Silva</span>
+							</div>
 						</div>
-						<div className="info-stat">
-							<span>T.Silva</span>
+
+						<div className="form">
+							<div className="info-name">
+								<span>Team</span>
+							</div>
+							<div className="info-stat">
+								<span>CHE</span>
+							</div>
+						</div>
+
+						<div className="form">
+							<div className="info-name">
+								<span>Form</span>
+							</div>
+							<div className="info-stat">
+								<span>8.0</span>
+							</div>
+						</div>
+
+						<div className="threat">
+							<div className="info-name">
+								<span>Threat</span>
+							</div>
+							<div className="info-stat">
+								<span>230</span>
+							</div>
+						</div>
+
+						<div className="opponent">
+							<div className="info-name">
+								<span>Opponent</span>
+							</div>
+							<div className="info-stat">
+								<span>NOR(H)</span>
+							</div>
+						</div>
+
+						<div className="opponentStrength">
+							<div className="info-name">
+								<span>Opponent Strength</span>
+							</div>
+							<div className="info-stat">
+								<span>3</span>
+							</div>
 						</div>
 					</div>
+				</div>
 
-					<div className="form">
-						<div className="info-name">
-							<span>Team</span>
+				<div className="captain">
+					<div className="captain-image"></div>
+					<div className="captain-info">
+						<div className="form">
+							<div className="info-name">
+								<span>Name</span>
+							</div>
+							<div className="info-stat">
+								<span>T.Silva</span>
+							</div>
 						</div>
-						<div className="info-stat">
-							<span>CHE</span>
+
+						<div className="form">
+							<div className="info-name">
+								<span>Team</span>
+							</div>
+							<div className="info-stat">
+								<span>CHE</span>
+							</div>
+						</div>
+
+						<div className="form">
+							<div className="info-name">
+								<span>Form</span>
+							</div>
+							<div className="info-stat">
+								<span>8.0</span>
+							</div>
+						</div>
+
+						<div className="threat">
+							<div className="info-name">
+								<span>Threat</span>
+							</div>
+							<div className="info-stat">
+								<span>230</span>
+							</div>
+						</div>
+
+						<div className="opponent">
+							<div className="info-name">
+								<span>Opponent</span>
+							</div>
+							<div className="info-stat">
+								<span>NOR(H)</span>
+							</div>
+						</div>
+
+						<div className="opponentStrength">
+							<div className="info-name">
+								<span>Opponent Strength</span>
+							</div>
+							<div className="info-stat">
+								<span>3</span>
+							</div>
 						</div>
 					</div>
+				</div>
 
-					<div className="form">
-						<div className="info-name">
-							<span>Form</span>
+				<div className="captain">
+					<div className="captain-image"></div>
+					<div className="captain-info">
+						<div className="form">
+							<div className="info-name">
+								<span>Name</span>
+							</div>
+							<div className="info-stat">
+								<span>T.Silva</span>
+							</div>
 						</div>
-						<div className="info-stat">
-							<span>8.0</span>
-						</div>
-					</div>
 
-					<div className="threat">
-						<div className="info-name">
-							<span>Threat</span>
+						<div className="form">
+							<div className="info-name">
+								<span>Team</span>
+							</div>
+							<div className="info-stat">
+								<span>CHE</span>
+							</div>
 						</div>
-						<div className="info-stat">
-							<span>230</span>
-						</div>
-					</div>
 
-					<div className="opponent">
-						<div className="info-name">
-							<span>Opponent</span>
+						<div className="form">
+							<div className="info-name">
+								<span>Form</span>
+							</div>
+							<div className="info-stat">
+								<span>8.0</span>
+							</div>
 						</div>
-						<div className="info-stat">
-							<span>NOR(H)</span>
-						</div>
-					</div>
 
-					<div className="opponentStrength">
-						<div className="info-name">
-							<span>Opponent Strength</span>
+						<div className="threat">
+							<div className="info-name">
+								<span>Threat</span>
+							</div>
+							<div className="info-stat">
+								<span>230</span>
+							</div>
 						</div>
-						<div className="info-stat">
-							<span>3</span>
+
+						<div className="opponent">
+							<div className="info-name">
+								<span>Opponent</span>
+							</div>
+							<div className="info-stat">
+								<span>NOR(H)</span>
+							</div>
+						</div>
+
+						<div className="opponentStrength">
+							<div className="info-name">
+								<span>Opponent Strength</span>
+							</div>
+							<div className="info-stat">
+								<span>3</span>
+							</div>
 						</div>
 					</div>
 				</div>
