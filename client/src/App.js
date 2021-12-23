47aedd7b9b82f1b7618a99cdfd6ba980b03d2ad4
Masho Ado <mashoado@gmail.com>
2021-12-23 22:36:47 +0300

    use useReducer to fetch data from our server

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,65 @@
-import React, { useEffect, useReducer } from "react"
-import axios from 'axios'
-import First11 from "./Components/First11"
-
+import React, { useEffect, useReducer } from "react";
+import axios from "axios";
+import First11 from "./Components/First11";
+import Captains from "./Components/Captains";
+import "./styles/ReusableStyles.css";
 
 function App() {
+	const initialState = {
+		loading: true,
+		teams: {},
+		players: {},
+		error: ``,
+	};
 
-  const initialState = {
-    loading: true,
-    teams: {},
-    players: {},
-    error: ``
-  }
+	const reducer = (state, action) => {
+		switch (action.type) {
+			case "FETCH_SUCCESS":
+				return {
+					loading: false,
+					teams: action.payload.teams,
+					players: action.payload.elements,
+					error: "",
+				};
+			case "FETCH_ERROR":
+				return {
+					loading: false,
+					teams: {},
+					players: {},
+					error: `${action.error}`,
+				};
+			default:
+				return state;
+		}
+	};
 
-  const reducer = ((state, action) => {
-    switch(action.type) {
-      case "FETCH_SUCCESS":
-        return {
-          loading: false,
-          teams: action.payload.teams,
-          players: action.payload.elements,
-          error: ""
-        }
-      case "FETCH_ERROR":
-        return {
-          loading: false,
-          teams: {},
-          players: {},
-          error: `${action.error}`
-        }
-      default:
-        return state
-    }
-  })
+	// fetch data from our server
+	useEffect(() => {
+		axios("http://localhost:4001/fetchbootstrap")
+			.then((res) => {
+				var data = res.data;
+				dispatch({
+					type: "FETCH_SUCCESS",
+					payload: { teams: data.teams, elements: data.elements },
+				});
+			})
+			.catch((err) => {
+				dispatch({ type: "FETCH_ERROR", error: err });
+			});
+	}, []);
 
+	const [state, dispatch] = useReducer(reducer, initialState);
+	const { teams, players, loading, error } = state;
+	console.log(state.players);
 
-  // fetch data from our server
-  useEffect(() => {
-    axios('http://localhost:4001/fetchbootstrap')
-    .then(res =>{
-      var data = res.data
-      dispatch({type:"FETCH_SUCCESS", payload : { teams : data.teams, elements : data.elements}})
-        
-    })     
-    .catch(err => {
-      dispatch({type: "FETCH_ERROR", error: err})
-    })
-   
-  }, [])
-
-const [state, dispatch] = useReducer(reducer, initialState)
-const {teams,players,loading,error} = state
-console.log(players)
-// getting players in specific position and sorting them based on form.
-// var goalkeepers = players.filter(player => player.element_type === 1).sort((a,b) => {
-//   return b.form - a.form 
-// }).slice(0,10)
-// goalkeepers = goalkeepers.sort((a,b) => b.points_per_game - a.points_per_game)
-
-// console.log(goalkeepers)
-
-
-
-  return (
-    <div className="App">
-      <First11 />
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* {error ? <h1>Error Occurred please refresh</h1> : null} */}
+			{/* {loading ? "Loading" : <First11 />} */}
+			<First11 />
+			<Captains />
+		</div>
+	);
 }
 
 export default App;
