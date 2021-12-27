d88399833157c1e21868941ca5e98fd0590ed4ab
Masho Ado <mashoado@gmail.com>
2021-12-27 14:31:44 +0300

    restructure

--- client/src/Components/Captains.js
+++ client/src/Components/Captains.js
@@ -1,19 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
+import "../styles/ReusableStyles.css";
 import "../styles/Captains.css";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Thumbs } from "swiper";
 import "swiper/swiper-bundle.css";
 
-SwiperCore.use([Thumbs]);
 function Captains() {
-	const [thumbsSwiper, setThumbsSwiper] = useState(null);
 	return (
-		<section className="captains-section">
+		<section className="players-section">
 			<h1 className="section-heading">Top 3 Captains this Week</h1>
 			<Swiper
 				className="captains-container"
 				slidesPerView={1.3}
-				onSwiper={setThumbsSwiper}
 				breakpoints={{
 					// set breakpoints to enhance responsiveness
 					// when window width is >= 750px
@@ -23,78 +20,19 @@ function Captains() {
 					// when window width is >= 950px
 					950: {
 						slidesPerView: 3,
+						// spaceBetween: "even",
 					},
 				}}
 			>
 				<SwiperSlide>
 					<div className="captain">
 						<div className="captain-image"></div>
-						<div className="captain-info">
+						<div className="player-info">
 							<div className="player-name-container">
 								<div className="info-name">
 									<span>Name</span>
 								</div>
-								<div className="info-stat">
-									<span>T.Silva</span>
-								</div>
-							</div>
-
-							<div className="team-container">
-								<div className="info-name">
-									<span>Team</span>
-								</div>
-								<div className="info-stat">
-									<span>CHE</span>
-								</div>
-							</div>
-
-							<div className="form">
-								<div className="info-name">
-									<span>Form</span>
-								</div>
-								<div className="info-stat">
-									<span>8.0</span>
-								</div>
-							</div>
-
-							<div className="threat">
-								<div className="info-name">
-									<span>Threat</span>
-								</div>
-								<div className="info-stat">
-									<span>230</span>
-								</div>
-							</div>
-
-							<div className="opponent">
-								<div className="info-name">
-									<span>Opponent</span>
-								</div>
-								<div className="info-stat">
-									<span>NOR(H)</span>
-								</div>
-							</div>
-
-							<div className="opponentStrength">
-								<div className="info-name">
-									<span>Opp. Strength</span>
-								</div>
-								<div className="info-stat">
-									<span>3</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</SwiperSlide>
-				<SwiperSlide tag="div">
-					<div className="captain">
-						<div className="captain-image"></div>
-						<div className="captain-info">
-							<div className="player-name-container">
-								<div className="info-name">
-									<span>Name</span>
-								</div>
-								<div className="info-stat">
+								<div className="player-name">
 									<span>T.Silva</span>
 								</div>
 							</div>
@@ -147,15 +85,76 @@ function Captains() {
 					</div>
 				</SwiperSlide>
 
-				<SwiperSlide tag="div">
+				<SwiperSlide>
 					<div className="captain">
-						<div className="captain-image"></div>
-						<div className="captain-info">
+						<div className="player-image captain-image"></div>
+						<div className="player-info">
 							<div className="player-name-container">
 								<div className="info-name">
 									<span>Name</span>
 								</div>
+								<div className="player-name">
+									<span>T.Silva</span>
+								</div>
+							</div>
+
+							<div className="team-container">
+								<div className="info-name">
+									<span>Team</span>
+								</div>
 								<div className="info-stat">
+									<span>CHE</span>
+								</div>
+							</div>
+
+							<div className="form">
+								<div className="info-name">
+									<span>Form</span>
+								</div>
+								<div className="info-stat">
+									<span>8.0</span>
+								</div>
+							</div>
+
+							<div className="threat">
+								<div className="info-name">
+									<span>Threat</span>
+								</div>
+								<div className="info-stat">
+									<span>230</span>
+								</div>
+							</div>
+
+							<div className="opponent">
+								<div className="info-name">
+									<span>Opponent</span>
+								</div>
+								<div className="info-stat">
+									<span>NOR(H)</span>
+								</div>
+							</div>
+
+							<div className="opponentStrength">
+								<div className="info-name">
+									<span>Opp. Strength</span>
+								</div>
+								<div className="info-stat">
+									<span>3</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</SwiperSlide>
+
+				<SwiperSlide>
+					<div className="captain">
+						<div className="player-image captain-image"></div>
+						<div className="player-info">
+							<div className="player-name-container">
+								<div className="info-name">
+									<span>Name</span>
+								</div>
+								<div className="player-name">
 									<span>T.Silva</span>
 								</div>
 							</div>
--- client/src/Components/First11.js
+++ client/src/Components/First11.js
@@ -18,15 +18,14 @@ function First11() {
 						<span className="corner corner-top-right">
 							{/* <!-- the top right corner --> */}
 						</span>
-
 						<div className="goalBox">
 							{/* <!-- the goal box goes here --> */}
 							<div className="goalkeeper-container">
 								<div className="goalkeeper">
 									<img src="" alt="" />
-									<div className="player-info">
-										<div className="player-name">Mendy</div>
-										<div className="next-opponent">WHU(A)</div>
+									<div className="f11-player-info">
+										<div className="f11-player-name">Mendy</div>
+										<div className="f11-next-opponent">WHU(A)</div>
 									</div>
 								</div>
 							</div>
@@ -34,86 +33,87 @@ function First11() {
 						<div className="defender-container">
 							<div className="defender">
 								<img src="" alt="" />
-								<div className="player-info">
-									<div className="player-name">Cancelo</div>
-									<div className="next-opponent">WHU(A)</div>
+								<div className="f11-player-info">
+									<div className="f11-player-name">Cancelo</div>
+									<div className="f11-next-opponent">WHU(A)</div>
 								</div>
 							</div>
 
 							<div className="defender">
 								<img src="" alt="" />
-								<div className="player-info">
-									<div className="player-name">James</div>
-									<div className="next-opponent">WHU(A)</div>
+								<div className="f11-player-info">
+									<div className="f11-player-name">James</div>
+									<div className="f11-next-opponent">WHU(A)</div>
 								</div>
 							</div>
 
 							<div className="defender">
 								<img src="" alt="" />
-								<div className="player-info">
-									<div className="player-name">Livramento</div>
-									<div className="next-opponent">WHU(A)</div>
+								<div className="f11-player-info">
+									<div className="f11-player-name">Livramento</div>
+									<div className="f11-next-opponent">WHU(A)</div>
 								</div>
 							</div>
 
 							<div className="defender">
 								<img src="" alt="" />
-								<div className="player-info">
-									<div className="player-name">Trent</div>
-									<div className="next-opponent">WHU(A)</div>
+								<div className="f11-player-info">
+									<div className="f11-player-name">Trent</div>
+									<div className="f11-next-opponent">WHU(A)</div>
 								</div>
 							</div>
 						</div>
 						<div className="midfielders-container">
 							<div className="midfielders">
 								<img src="" alt="" />
-								<div className="player-info">
-									<div className="player-name">Gilmour</div>
-									<div className="next-opponent">WHU(A)</div>
+								<div className="f11-player-info">
+									<div className="f11-player-name">Gilmour</div>
+									<div className="f11-next-opponent">WHU(A)</div>
 								</div>
 							</div>
 
 							<div className="midfielders">
 								<img src="" alt="" />
-								<div className="player-info">
-									<div className="player-name">Son</div>
-									<div className="next-opponent">WHU(A)</div>
+								<div className="f11-player-info">
+									<div className="f11-player-name">Son</div>
+									<div className="f11-next-opponent">WHU(A)</div>
 								</div>
 							</div>
 
 							<div className="midfielders">
 								<img src="" alt="" />
-								<div className="player-info">
-									<div className="player-name">Salah</div>
-									<div className="next-opponent">WHU(A)</div>
+								<div className="f11-player-info">
+									<div className="f11-player-name">Salah</div>
+									<div className="f11-next-opponent">WHU(A)</div>
 								</div>
 							</div>
 
 							<div className="midfielders">
 								<img src="" alt="" />
-								<div className="player-info">
-									<div className="player-name">Gallagher</div>
-									<div className="next-opponent">WHU(A)</div>
+								<div className="f11-player-info">
+									<div className="f11-player-name">Gallagher</div>
+									<div className="f11-next-opponent">WHU(A)</div>
 								</div>
 							</div>
 						</div>
 						<div className="fowards-container">
 							<div className="fowards">
 								<img src="" alt="" />
-								<div className="player-info">
-									<div className="player-name">Vardy</div>
-									<div className="next-opponent">WHU(A)</div>
+								<div className="f11-player-info">
+									<div className="f11-player-name">Vardy</div>
+									<div className="f11-next-opponent">WHU(A)</div>
 								</div>
 							</div>
 
 							<div className="fowards">
 								<img src="" alt="" />
-								<div className="player-info">
-									<div className="player-name">Jimenez</div>
-									<div className="next-opponent">WHU(A)</div>
+								<div className="f11-player-info">
+									<div className="f11-player-name">Jimenez</div>
+									<div className="f11-next-opponent">WHU(A)</div>
 								</div>
 							</div>
 						</div>
+
 						<div className="half">
 							{/* <!-- the half way point of the pitch is here --> */}
 						</div>
