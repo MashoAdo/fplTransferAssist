91c941a350f36eb0225122be3b83841fd0fb6eb9
Masho Ado <mashoado@gmail.com>
2021-12-29 01:08:21 +0300

    structure the long term player components and its table data components

---
+++ client/src/Components/LongtermPlayers.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "../styles/LongTermPlayers.css";
+
+function LongtermPlayers() {
+	return (
+		<section className="longTerm-players">
+			<h1 className="section-heading">Long Term Players</h1>
+			<table>
+				<tr>
+					<th>Player Name</th>
+					<th>Form</th>
+					<th>Next Opp</th>
+					<th>Opps FDR</th>
+					<th>Combined Opp. FDR</th>
+				</tr>
+
+				<tr>
+					<td>Hazard</td>
+					<td>9.0</td>
+					<td>WHU(H),BUR(A),CRY(H)</td>
+					<td>4,2,3</td>
+					<td>9</td>
+				</tr>
+
+				<tr>
+					<td>Salah</td>
+					<td>8.6</td>
+					<td>WHU(H),BUR(A),CRY(H)</td>
+					<td>4,2,3</td>
+					<td>9</td>
+				</tr>
+
+				<tr>
+					<td>Gallagher</td>
+					<td>8.6</td>
+					<td>WHU(H),BUR(A),CRY(H)</td>
+					<td>4,2,3</td>
+					<td>9</td>
+				</tr>
+
+				<tr>
+					<td>Hazard</td>
+					<td>9.0</td>
+					<td>WHU(H),BUR(A),CRY(H)</td>
+					<td>4,2,3</td>
+					<td>9</td>
+				</tr>
+
+				<tr>
+					<td>Salah</td>
+					<td>8.6</td>
+					<td>WHU(H),BUR(A),CRY(H)</td>
+					<td>4,2,3</td>
+					<td>9</td>
+				</tr>
+
+				<tr>
+					<td>Gallagher</td>
+					<td>8.6</td>
+					<td>WHU(H),BUR(A),CRY(H)</td>
+					<td>4,2,3</td>
+					<td>9</td>
+				</tr>
+			</table>
+		</section>
+	);
+}
+
+export default LongtermPlayers;
