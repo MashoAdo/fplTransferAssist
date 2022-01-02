04ab220560ea3938bda991d49e6a39227e8a9ae7
Masho Ado <mashoado@gmail.com>
2022-01-02 16:14:13 +0300

    add dynamic data fetched from backend

--- client/src/Components/LongtermPlayers.js
+++ client/src/Components/LongtermPlayers.js
@@ -1,66 +1,46 @@
 import React from "react";
 import "../styles/LongTermPlayers.css";
 
-function LongtermPlayers() {
+function LongtermPlayers({ longTermPlayers }) {
+	// var finalLongtermPlayers = longtermPlayers.
+
+	console.log(longTermPlayers);
 	return (
 		<section className="longTerm-players">
 			<h1 className="section-heading">Long Term Players</h1>
 			<table>
-				<tr>
-					<th>Player Name</th>
-					<th>Form</th>
-					<th>Next Opp</th>
-					<th>Opps FDR</th>
-					<th>Combined Opp. FDR</th>
-				</tr>
+				<thead>
+					<tr>
+						<th>Player Name</th>
+						<th>Player Team</th>
+						<th>Form</th>
+						<th>Next Opp</th>
+						<th>Opps FDR</th>
+						<th>Cumulative Opp. FDR</th>
+					</tr>
+				</thead>
 
-				<tr>
-					<td>Hazard</td>
-					<td>9.0</td>
-					<td>WHU(H),BUR(A),CRY(H)</td>
-					<td>4,2,3</td>
-					<td>9</td>
-				</tr>
-
-				<tr>
-					<td>Salah</td>
-					<td>8.6</td>
-					<td>WHU(H),BUR(A),CRY(H)</td>
-					<td>4,2,3</td>
-					<td>9</td>
-				</tr>
-
-				<tr>
-					<td>Gallagher</td>
-					<td>8.6</td>
-					<td>WHU(H),BUR(A),CRY(H)</td>
-					<td>4,2,3</td>
-					<td>9</td>
-				</tr>
-
-				<tr>
-					<td>Hazard</td>
-					<td>9.0</td>
-					<td>WHU(H),BUR(A),CRY(H)</td>
-					<td>4,2,3</td>
-					<td>9</td>
-				</tr>
-
-				<tr>
-					<td>Salah</td>
-					<td>8.6</td>
-					<td>WHU(H),BUR(A),CRY(H)</td>
-					<td>4,2,3</td>
-					<td>9</td>
-				</tr>
-
-				<tr>
-					<td>Gallagher</td>
-					<td>8.6</td>
-					<td>WHU(H),BUR(A),CRY(H)</td>
-					<td>4,2,3</td>
-					<td>9</td>
-				</tr>
+				<tbody>
+					{longTermPlayers.map((player, index) => {
+						return (
+							<tr key={index}>
+								<td>{player.web_name}</td>
+								<td>{player.teamName}</td>
+								<td>{player.form}</td>
+								<td>{player.opponentsName}</td>
+								<td>{player.opponentStrength}</td>
+								<td>{player.cumulativeOpponentStrength}</td>
+							</tr>
+						);
+					})}
+					{/* <tr>
+						<td>Hazard</td>
+						<td>9.0</td>
+						<td>WHU(H),BUR(A),CRY(H)</td>
+						<td>4,2,3</td>
+						<td>9</td>
+					</tr> */}
+				</tbody>
 			</table>
 		</section>
 	);
