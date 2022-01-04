651e988d72107b6459d59d64f7350de2d8a65b7b
Masho Ado <mashoado@gmail.com>
2022-01-04 15:04:16 +0300

    refactore

--- server.js
+++ server.js
@@ -39,12 +39,12 @@ if (process.env.NODE_ENV === "production") {
 			}
 		);
 	});
-}
 
-// All other GET requests not handled before will return our React app
-app.get("/*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
-});
+	// All other GET requests not handled before will return our React app
+	app.get("/*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
+	});
+}
 
 app.listen(PORT, () => {
 	console.log(`server is running at port ${[PORT]}`);
