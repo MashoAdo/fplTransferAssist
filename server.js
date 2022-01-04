da0802ed530218836ace3cafa0889fc9f72a06ee
Masho Ado <mashoado@gmail.com>
2022-01-04 14:13:24 +0300

    add production code

--- server.js
+++ server.js
@@ -1,42 +1,51 @@
 const express = require("express");
+const path = require("path");
 const request = require("request");
 const cors = require("cors");
+const PORT = process.env.PORT || 8000;
 
 const app = express();
-// var corsOptions = {
-// 	origin: "http://127.0.0.1:5500/client/public/index.html",
-// };
 app.use(cors());
 
-app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html");
-});
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-app.get("/fetchbootstrap", (req, res) => {
-	request(
-		"https://fantasy.premierleague.com/api/bootstrap-static/",
-		function (error, response, body) {
-			if (error || response.statusCode !== 200) {
-				return res.status(500).json({ type: "error", message: error.message });
+	//FIRST Api endpoints gives priority to our backend so that node serves data fetched from our request
+	app.get("/fetchbootstrap", (req, res) => {
+		request(
+			"https://fantasy.premierleague.com/api/bootstrap-static/",
+			function (error, response, body) {
+				if (error || response.statusCode !== 200) {
+					return res
+						.status(500)
+						.json({ type: "error", message: error.message });
+				}
+
+				res.send(body);
 			}
+		);
+	});
 
-			res.send(body);
-		}
-	);
-});
-
-app.get("/fixtures", (req, res) => {
-	request(
-		"https://fantasy.premierleague.com/api/fixtures/?future=1",
-		(error, response, body) => {
-			if (error || response.statusCode !== 200) {
-				return res.status(500).json({ typer: "error", message: error.message });
+	app.get("/fixtures", (req, res) => {
+		request(
+			"https://fantasy.premierleague.com/api/fixtures/?future=1",
+			(error, response, body) => {
+				if (error || response.statusCode !== 200) {
+					return res
+						.status(500)
+						.json({ typer: "error", message: error.message });
+				}
+				res.send(body);
 			}
-			res.send(body);
-		}
-	);
+		);
+	});
+}
+
+// All other GET requests not handled before will return our React app
+app.get("/*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
-app.listen(4001, () => {
-	console.log("server is running at port 4001");
+app.listen(PORT, () => {
+	console.log(`server is running at port ${[PORT]}`);
 });
